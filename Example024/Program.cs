e549148da8dd62accb02e8d8eb30903a7fc87f44
korobov_o <korobok13@yandex.ru>
2022-11-13 14:13:12 +0300

    четвертый пример. массив

---
+++ Example024/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {1, 7, 2, 3, 4, 7, 5, 6, 7, 8};
+
+int n = array.Length;
+int find = 7;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+
+}
